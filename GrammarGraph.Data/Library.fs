83c6e3e8e7d50d1f1c86d15ea647eb5afee253cd
Peter-B- <peter@butzhammer.de>
2023-07-14 22:34:04 +0200

    Add some raw data files from ggplot repository and try to load them

---
+++ GrammarGraph.Data/Library.fs
@@ -0,0 +1,24 @@
+﻿namespace GrammarGraph.Data
+
+open System.Reflection
+open FSharp.Data
+
+module DataSets =
+    type internal Marker = interface end
+
+    let loadDataSet name = 
+        let t = typeof<Marker>.DeclaringType
+        let assembly = Assembly.GetAssembly(t)
+
+        let stream = assembly.GetManifestResourceStream($"GrammarGraph.Data.DataSets.{name}.csv")
+        use reader = new System.IO.StreamReader(stream)
+        reader.ReadToEnd()
+
+
+    let diamonds = 
+        let csv = loadDataSet "diamonds"
+        let file = CsvFile.Parse( csv)
+        file.Rows
+        |> Seq.map (fun row -> row.["carat"].AsFloat())
+            
+            
---
+++ GrammarGraph.Test/Program.fs
@@ -0,0 +1,10 @@
+﻿open GrammarGraph.Data
+
+
+
+let diamonds = 
+    DataSets.diamonds
+    |> Seq.truncate 10
+
+for d in diamonds do
+    printfn "%f" d
