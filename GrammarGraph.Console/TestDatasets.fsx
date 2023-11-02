a92530cb5954f6c200704f64aac071a9cd0f6b6f
Peter-B- <peter@butzhammer.de>
2023-11-02 19:40:36 +0100

    Remove F# projects

--- GrammarGraph.Console/TestDatasets.fsx
+++
@@ -1,8 +0,0 @@
-﻿
-#r "../GrammarGraph.Data/bin/Debug/net7.0/GrammarGraph.Data.dll"
-open GrammarGraph.Data
-
-DataSets.fuelEconomy()
-|> Seq.groupBy (fun fe -> fe.Transmission)
-|> Seq.map (fun (key,gr) -> $"{key}: {gr|> Seq.length}")
-|> Seq.iter (printfn "%s")
